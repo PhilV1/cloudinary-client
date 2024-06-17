40675559c677f3edabfa82e3b9d59eeb427e03da
PhilV1 <werpub.phil@gmx.de>
2024-06-17 11:46:04 +0200

    feat: Add carousel with images to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,37 @@ function App() {
         React-Dinary is a dynamic, user-friendly website designed for showcasing
         and managing product images with ease.
       </h2>
+      <div className="mt-6 carousel w-full ">
+        <div id="item1" className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
+            className="w-full"
+          />
+        </div>
+        <div id="item2" className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+            className="w-full"
+          />
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+            className="w-full"
+          />
+        </div>
+      </div>
+      <div className="flex justify-center w-full  py-2 gap-2">
+        <a href="#item1" className="btn btn-xs">
+          1
+        </a>
+        <a href="#item2" className="btn btn-xs">
+          2
+        </a>
+        <a href="#item3" className="btn btn-xs">
+          3
+        </a>
+      </div>
     </>
   )
 }
--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -3,23 +3,26 @@ import { NavLink, Outlet } from 'react-router-dom'
 const NavComponent = () => {
   return (
     <>
-      <nav className=" bg-base-100 py-3 lg:px-20 shadow-lg shadow-primary mb-6">
+      <nav className=" bg-base-100 py-3 lg:px-20 shadow-lg shadow-primary mb-6 px-4">
         <div className="flex justify-between">
           <div className="">
-            <NavLink to="/" className="btn btn-ghost text-xl">
+            <NavLink
+              to="/"
+              className="btn btn-ghost text-xl [&.active]:text-primary"
+            >
               REACT-Dinary
             </NavLink>
           </div>
           <div>
             <NavLink
               to="/images"
-              className="btn btn-ghost text-xl [&.active]:text-white"
+              className="btn btn-ghost text-xl [&.active]:text-primary"
             >
               Images
             </NavLink>
             <NavLink
               to="/product"
-              className="btn btn-ghost text-xl [&.active]:text-white"
+              className="btn btn-ghost text-xl [&.active]:text-primary"
             >
               Products
             </NavLink>
