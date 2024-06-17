6c675c20b5d91270ef3d1fff498139ddc37f44ec
PhilV1 <werpub.phil@gmx.de>
2024-06-17 13:47:08 +0200

    refactor: Update ThemeToggler component to use tailwind CSS classes for button styling

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,38 +1,28 @@
-// Icons und useState importieren
 import { BsMoonFill, BsSunFill } from 'react-icons/bs'
 import { useState } from 'react'
 
-// Themes Objekt erstellen mit den Themes die du in deinem tailwind.config.js Datei eingefügt hast.
 const themes = {
   light: 'light',
   dark: 'dark',
 }
 
-// ThemeToggle Komponente erstellen
 const ThemeToggle = () => {
-  // useState für das Theme - default ist light
   const [theme, setTheme] = useState(themes.light)
 
-  // Funktion um das Theme zu wechseln
-  // Wenn das Theme light ist, dann wechsel zu dark und umgekehrt
   const toggleTheme = () => {
-    // Wenn das Theme light ist, dann wechsel zu dark und umgekehrt und speichere das Theme in der newTheme Variable
     const newTheme = theme === themes.light ? themes.dark : themes.light
 
-    // Setze das Theme in der HTML Attribute und ...
     document.documentElement.setAttribute('data-theme', newTheme)
-    // Setze das Theme in der useState Variable
+
     setTheme(newTheme)
   }
 
-  // Button mit dem onClick Event und dem Icon
   return (
-    <button onClick={toggleTheme} className="btn btn-sm">
-      {/* Wenn das Theme light ist, dann zeige das Moon Icon und umgekehrt */}
+    <button onClick={toggleTheme} className="btn ">
       {theme === 'light' ? (
-        <BsMoonFill className="h-4 w-4 " />
+        <BsMoonFill className="h-5 w-5 " />
       ) : (
-        <BsSunFill className="h-4 w-4" />
+        <BsSunFill className="h-5 w-5" />
       )}
     </button>
   )
